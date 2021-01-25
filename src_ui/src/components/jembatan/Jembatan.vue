963acd6fdb984ae9d6075cb66e31d72f911d32f7
userbaruu <fulang.gulan@gmail.com>
2021-01-25 16:16:25 +0700

    membuat komponen baru untuk jembatan

---
+++ src_ui/src/components/jembatan/Jembatan.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="q-px-md">
+    <canvas ref="jembatan" class="full-width" style="height: 650px" />
+    <q-resize-observer @resize="onResize" />
+  </div>
+</template>
+
+<script src="./jembatanHandler.js"/>
