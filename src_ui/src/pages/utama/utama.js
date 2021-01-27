345463a3987cbb36b70382749b165057e689a26f
userbaruu <fulang.gulan@gmail.com>
2021-01-27 08:21:07 +0700

    membuat fungsi external link

--- src_ui/src/pages/utama/utama.js
+++ src_ui/src/pages/utama/utama.js
@@ -1,8 +1,22 @@
+import { openURL } from 'quasar'
 import Jembatan from 'src/components/jembatan/Jembatan'
 
 export default {
   name: 'PageIndex',
   components: {
     Jembatan
+  },
+  data () {
+    return {
+      jembatanHeight: 0
+    }
+  },
+  mounted () {
+    this.jembatanHeight = window.innerHeight - 82
+  },
+  methods: {
+    goToExternal (url) {
+      openURL(url, null, { noopener: true, noreferrer: true })
+    }
   }
 }
