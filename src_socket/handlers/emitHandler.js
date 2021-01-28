b50acdf984bfe63ac062c82e9773dcbac0e7f23b
userbaruu <fulang.gulan@gmail.com>
2021-01-28 07:11:35 +0700

    membuat handler untuk emit sensor dari socket

---
+++ src_socket/handlers/emitHandler.js
@@ -0,0 +1,12 @@
+const emitSensor = (io) => {
+  setInterval(() => {
+    io.emit(process.env.SOCKET_ROOM_DEFAULT, {
+      acc: Math.random().toFixed(3),
+      strain1: Math.random().toFixed(3),
+      strain2: Math.random().toFixed(3),
+      disp: Math.random().toFixed(3)
+    })
+  }, 1000)
+}
+
+module.exports = { emitSensor }
