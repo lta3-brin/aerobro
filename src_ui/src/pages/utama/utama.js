70cafd58d28f3baf9ea33b6e164fd58c2ec571d6
userbaruu <fulang.gulan@gmail.com>
2021-01-29 07:55:30 +0700

    computed variable untuk status bacaan sensor dinamik

--- src_ui/src/pages/utama/utama.js
+++ src_ui/src/pages/utama/utama.js
@@ -28,6 +28,39 @@ export default {
   computed: {
     sensor () {
       return this.$store.getters['jembatan/sensorGetter']
+    },
+    accSignal () {
+      const acc = this.sensor.acc
+
+      if (acc <= process.env.ACC_THRESHOLD_MIN) {
+        return 'positive'
+      } else if (acc > process.env.ACC_THRESHOLD_MIN && acc <= process.env.ACC_THRESHOLD_MAX) {
+        return 'warning'
+      } else {
+        return 'negative'
+      }
+    },
+    strainSignal1 () {
+      const strain1 = this.sensor.strain1
+
+      if (strain1 <= process.env.STRAIN_THRESHOLD_MIN) {
+        return 'positive'
+      } else if (strain1 > process.env.STRAIN_THRESHOLD_MIN && strain1 <= process.env.STRAIN_THRESHOLD_MAX) {
+        return 'warning'
+      } else {
+        return 'negative'
+      }
+    },
+    strainSignal2 () {
+      const strain2 = this.sensor.strain2
+
+      if (strain2 <= process.env.STRAIN_THRESHOLD_MIN) {
+        return 'positive'
+      } else if (strain2 > process.env.STRAIN_THRESHOLD_MIN && strain2 <= process.env.STRAIN_THRESHOLD_MAX) {
+        return 'warning'
+      } else {
+        return 'negative'
+      }
     }
   }
 }
