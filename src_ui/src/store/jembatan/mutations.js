578e0e011db1ac4f0827a92d6358ed4960233ac3
userbaruu <fulan.gulan@gmail.com>
2021-02-01 15:42:30 +0700

    hapus state mutation yang tidak digunakan

--- src_ui/src/store/jembatan/mutations.js
+++ src_ui/src/store/jembatan/mutations.js
@@ -5,12 +5,3 @@ export function cameraMutation (state, cam) {
 export function sensorMutation (state, payload) {
   state.sensor = payload
 }
-
-export function sensorOfflineMutation (state) {
-  state.sensor = {
-    sinyal: 'offline',
-    acc: 0,
-    strain1: 0,
-    strain2: 0
-  }
-}
