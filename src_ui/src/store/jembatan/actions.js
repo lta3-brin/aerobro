a59b49d19d2a8c181545fcdae34d4a0c1e628cfc
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-02-03 06:52:23 +0700

    Membuat fungsi state action untuk versi aplikasi

--- src_ui/src/store/jembatan/actions.js
+++ src_ui/src/store/jembatan/actions.js
@@ -1,2 +1,8 @@
-export function cameraAction (/* context */) {
+import axios from 'axios'
+
+export async function versionAction () {
+  const repo = process.env.REPO_NAME
+  const result = await axios.get(`https://api.github.com/repos/bbta3-bppt/${repo}/tags`)
+
+  return result.data
 }
