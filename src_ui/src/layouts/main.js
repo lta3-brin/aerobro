9f336e650e8d5ea5ec72d3037ed9014d88a186fd
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-02-03 07:03:22 +0700

    membuat handler awal untuk layout utama

---
+++ src_ui/src/layouts/main.js
@@ -0,0 +1,25 @@
+export default {
+  name: 'MainLayout',
+  data () {
+    return {}
+  },
+  async created () {
+    await this.onCreate()
+  },
+  methods: {
+    async onCreate () {
+      try {
+        const result = await this.$store.dispatch('jembatan/versionAction')
+
+        this.$store.commit('jembatan/setVersionMutation', result[0].name)
+      } catch (e) {
+        console.log(e.message)
+      }
+    }
+  },
+  computed: {
+    version: function () {
+      return this.$store.getters['jembatan/versionGetter']
+    }
+  }
+}
