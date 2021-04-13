ec6b6a56b7558e183f54dfee9143d436b4bd7da6
userbaruu <fulan.gulan@gmail.com>
2021-04-13 12:17:12 +0700

    Hapus boot file yang tidak digunakan

--- ui/src/boot/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client'
-
-// "async" is optional;
-// more info on params: https://quasar.dev/quasar-cli/boot-files
-export default ({ Vue }) => {
-  Vue.prototype.$socket = io(process.env.SOCKET_ADDRESS)
-}
