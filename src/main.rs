4ce17ed82af991b9cdbe498df0672f4d26189bb7
userbaruu <fulan.gulan@gmail.com>
2021-04-14 07:01:56 +0700

    Modifikasi fungsi main menangani mqtt dan websocket

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,40 @@
 mod configs;
 mod errors;
+mod handlers;
 
 use dotenv::dotenv;
-use paho_mqtt as mqtt;
-use std::time::Duration;
+use std::thread::spawn;
+use std::net::TcpListener;
+use crossbeam_channel::unbounded;
+use tungstenite::{accept, Message};
 use crate::errors::AppErrors;
 use crate::configs::get_configs;
+use crate::handlers::run_mqtt;
 
 
 fn main() -> Result<(), AppErrors> {
     dotenv().ok();
 
+    let (s, r) = unbounded::<String>();
     let configs = get_configs()?;
-    let client_opts = mqtt::CreateOptionsBuilder::new()
-        .server_uri(configs.get_addr())
-        .client_id("aerobro_mqtt_subscriber")
-        .finalize();
+    let server = TcpListener::bind(configs.get_websocket_addr())?;
 
-    let mut client = mqtt::Client::new(client_opts)?;
-    let rx = client.start_consuming();
+    spawn(move || {
+        run_mqtt(configs, s)
+    });
 
-    let lwt = mqtt::MessageBuilder::new()
-        .topic("aerobro_topic")
-        .payload("Consumer lost connection")
-        .finalize();
+    for stream in server.incoming() {
+        let rclone = r.clone();
 
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(Duration::from_secs(20))
-        .clean_session(false)
-        .will_message(lwt)
-        .user_name(configs.get_user())
-        .password(configs.get_password())
-        .finalize();
+        spawn(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
 
-    client.connect(conn_opts)?;
-    client.subscribe(configs.get_topic().as_str(), 1)?;
-
-    println!("Menjalankan konversi protokol...");
-    for rx in rx.iter() {
-        if let Some(msg) = rx {
-            let payload = msg.payload();
-            println!("{}", std::str::from_utf8(payload)?)
-        }
+            loop {
+                let message = rclone.recv().unwrap();
+                websocket.write_message(Message::from(message)).unwrap();
+            }
+        });
     }
 
     Ok(())
 }
-
