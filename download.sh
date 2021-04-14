45ae8f762b9bfc3d32af75af4d376557328bb272
userbaruu <fulan.gulan@gmail.com>
2021-04-14 10:08:32 +0700

    Koreksi instruksi direktori simpan aplikasi

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@ DOWNLOAD_URL=$(curl -s https://api.github.com/repos/bbta3-bppt/aerobro/releases/
         | grep browser_download_url \
         | grep aerobro \
         | cut -d '"' -f 4)
-curl -s -L --create-dirs -o ~/www/aerobro/_work/aerobro/aerobro/aerobro "$DOWNLOAD_URL"
+curl -s -L --create-dirs -o ~/www/aerobro/_work/aerobro/aerobro "$DOWNLOAD_URL"
