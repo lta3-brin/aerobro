7a494d4066c98045413c4d60a0920fbf8e255548
userbaruu <fulan.gulan@gmail.com>
2021-04-14 08:36:53 +0700

    Hapus env yang tidak digunakan

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -75,7 +75,6 @@ module.exports = function (/* ctx */) {
       // https://quasar.dev/quasar-cli/handling-process-env#Import-based-on-process.env
       env: {
         SOCKET_ADDRESS: process.env.SOCKET_ADDRESS,
-        SOCKET_ROOM_DEFAULT: process.env.SOCKET_ROOM_DEFAULT,
         ACC_THRESHOLD_MIN: process.env.ACC_THRESHOLD_MIN,
         ACC_THRESHOLD_MAX: process.env.ACC_THRESHOLD_MAX,
         STRAIN_THRESHOLD_MIN: process.env.STRAIN_THRESHOLD_MIN,
