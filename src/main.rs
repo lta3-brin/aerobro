b0ae1f0cb7b7cffc00d24558504b2a4b4e4a5725
userbaruu <fulan.gulan@gmail.com>
2021-04-16 22:07:25 +0700

    Modifikasi terapan dalam proses utama

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,28 @@
 mod configs;
 mod errors;
+mod helpers;
 mod handlers;
 
-use std::fs::File;
-use std::io::Read;
-use std::sync::Arc;
 use dotenv::dotenv;
-use std::thread::spawn;
-use tungstenite::accept;
-use std::net::TcpListener;
-use crossbeam_channel::unbounded;
-use native_tls::{Identity, TlsAcceptor};
+use actix_web::{App, HttpServer};
+use crate::handlers::index;
 use crate::errors::AppErrors;
 use crate::configs::get_configs;
-use crate::handlers::{run_mqtt, send_message};
 
 
-fn main() -> Result<(), AppErrors> {
+#[actix_web::main]
+async fn main() -> Result<(), AppErrors> {
     dotenv().ok();
 
-    let (s, r) = unbounded::<String>();
     let configs = get_configs()?;
-    let server = TcpListener::bind(configs.get_websocket_addr())?;
-
-    spawn(move || {
-        run_mqtt(configs, s)
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(index)
     });
 
-    for stream in server.incoming() {
-        let rclone = r.clone();
-
-        spawn(move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-
-            loop {
-                match send_message(rclone.clone(), &mut websocket) {
-                    Ok(_) => (),
-                    Err(err) => eprintln!("{:?}", err)
-                }
-            }
-        });
-    }
+    server.bind(configs.get_app_addr())?
+        .run()
+        .await?;
 
     Ok(())
 }
