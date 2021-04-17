e18a1fd110949f2e9201c81d0670ff4ec80db010
userbaruu <fulan.gulan@gmail.com>
2021-04-17 08:43:20 +0700

    Membuat fungsi awal handler untuk websocket

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,15 @@
-use actix_web::{get, Responder};
+use actix_web_actors::ws;
+use actix_web::{get, Responder, HttpRequest, web};
+use crate::helpers::AerobroWs;
 
 #[get("/")]
 pub async fn index() -> impl Responder {
     format!("Halaman ini dikosongkan.")
+}
+
+#[get("/aerobrows")]
+pub async fn aerobrows(req: HttpRequest, stream: web::Payload) -> impl Responder {
+    let resp = ws::start(AerobroWs {}, &req, stream);
+
+    resp
 }
