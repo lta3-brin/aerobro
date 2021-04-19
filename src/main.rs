32d2868e185e13e8082bff56f2ce8d6b906f1320
userbaruu <fulan.gulan@gmail.com>
2021-04-19 10:22:15 +0700

    Modifikasi proses utama terkait concurrency, service dan cors

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod configs;
 mod errors;
 mod helpers;
 mod handlers;
+mod models;
 
 use dotenv::dotenv;
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
-use crate::handlers::{index, aerobrows};
+use crate::handlers::{index, bh77, bh77_broadcast};
 use crate::errors::AppErrors;
 use crate::configs::get_configs;
+use crate::models::Broadcasters;
 
 
 #[actix_web::main]
@@ -15,14 +18,20 @@ async fn main() -> Result<(), AppErrors> {
     dotenv().ok();
 
     let configs = get_configs()?;
-    let server = HttpServer::new(|| {
+    let data = Broadcasters::create();
+    let server = HttpServer::new(move || {
+        let cors = Cors::default().allow_any_origin().allow_any_header();
+
         App::new()
+            .wrap(cors)
+            .app_data(data.clone())
             .service(index)
-            .service(aerobrows)
+            .service(bh77)
+            .service(bh77_broadcast)
     });
 
-    println!("{}", format!("Menjalankan servis Aerobro di {}", configs.get_app_addr()));
-    server.bind(configs.get_app_addr())?
+    println!("{}", format!("Menjalankan servis Aerobro di 0.0.0.0:{}", configs.get_app_port()));
+    server.bind(format!("0.0.0.0:{}", configs.get_app_port()))?
         .run()
         .await?;
 
