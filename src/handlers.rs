4a905441df23234d7aa222b45d2277b04917058a
userbaruu <fulan.gulan@gmail.com>
2021-04-19 10:20:05 +0700

    Membuat fungsi baru untuk handle http event stream

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,15 +1,43 @@
-use actix_web_actors::ws;
-use actix_web::{get, Responder, HttpRequest, web};
-use crate::helpers::AerobroWs;
+use std::sync::Mutex;
+use actix_web::web::Data;
+use actix_web::{get, post, Responder, HttpResponse, web};
+use crate::models::{Broadcasters, Sensor};
+
 
 #[get("/")]
 pub async fn index() -> impl Responder {
     format!("Halaman ini dikosongkan.")
 }
 
-#[get("/aerobrows")]
-pub async fn aerobrows(req: HttpRequest, stream: web::Payload) -> impl Responder {
-    let resp = ws::start(AerobroWs {}, &req, stream);
+#[get("/bh77")]
+pub async fn bh77(data: Data<Mutex<Broadcasters>>) -> impl Responder {
+    match data.lock() {
+        Ok(mut r) => {
+            HttpResponse::Ok()
+                .header("content-type", "text/event-stream")
+                .streaming(r.new_client())
+        }
+        Err(err) => {
+            HttpResponse::BadRequest().body(err.to_string())
+        }
+    }
+}
 
-    resp
+#[post("/bh77")]
+pub async fn bh77_broadcast(
+    msg: web::Json<Sensor>,
+    data: Data<Mutex<Broadcasters>>,
+) -> impl Responder {
+    match data.lock() {
+        Ok(dt) => {
+            dt.send_sensor(msg.0.value.as_str());
+
+            HttpResponse::Ok().body("Data sensor dikirimkan")
+        }
+        Err(e) => {
+            HttpResponse::BadGateway().body(
+                format!("{}", e)
+            )
+        }
+    }
 }
