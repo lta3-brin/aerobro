bb8d45c0fb40d943450221c38795cebd052d49a1
userbaruu <fulan.gulan@gmail.com>
2021-05-10 22:05:04 +0700

    Modifikasi lanjut package handler dengan fungsi olah data sesuai waktu

--- proc/handler.py
+++ proc/handler.py
@@ -1,12 +1,10 @@
-import pandas as pd
-from datetime import datetime
 import numpy as np
-
-np.random.seed(101)
+import pandas as pd
 
 
 def calc_data(when):
     from proc.helper import acc_status, displ_status, strn_status
+    np.random.seed(101)
 
     date_rng = pd.date_range(start='1/1/2018', end='1/12/2021', freq='H')
     df = pd.DataFrame(date_rng, columns=['date'])
@@ -33,6 +31,63 @@ def calc_data(when):
 
         df_strn = df.groupby([
             df["date"].dt.to_period("D"),
+            df["strn_status"]
+        ]).size().reset_index(name="count")
+        print(df_strn)
+
+    elif when == "weekly":
+        df_acc = df.groupby([
+            df["date"].dt.to_period("W"),
+            df["acc_status"]
+        ]).size().reset_index(name="count")
+        print(df_acc)
+
+        df_displ = df.groupby([
+            df["date"].dt.to_period("W"),
+            df["displ_status"]
+        ]).size().reset_index(name="count")
+        print(df_displ)
+
+        df_strn = df.groupby([
+            df["date"].dt.to_period("W"),
+            df["strn_status"]
+        ]).size().reset_index(name="count")
+        print(df_strn)
+
+    elif when == "monthly":
+        df_acc = df.groupby([
+            df["date"].dt.to_period("M"),
+            df["acc_status"]
+        ]).size().reset_index(name="count")
+        print(df_acc)
+
+        df_displ = df.groupby([
+            df["date"].dt.to_period("M"),
+            df["displ_status"]
+        ]).size().reset_index(name="count")
+        print(df_displ)
+
+        df_strn = df.groupby([
+            df["date"].dt.to_period("M"),
+            df["strn_status"]
+        ]).size().reset_index(name="count")
+        print(df_strn)
+
+    elif when == "yearly":
+        df_acc = df.groupby([
+            df["date"].dt.to_period("Y"),
+            df["acc_status"]
+        ]).size().reset_index(name="count")
+        print(df_acc)
+
+        df_displ = df.groupby([
+            df["date"].dt.to_period("Y"),
+            df["displ_status"]
+        ]).size().reset_index(name="count")
+        print(df_displ)
+
+        df_strn = df.groupby([
+            df["date"].dt.to_period("Y"),
             df["strn_status"]
         ]).size().reset_index(name="count")
         print(df_strn)
