6d4df16ad78e00b480cd3d88b836a640ca7b0c26
userbaruu <fulan.gulan@gmail.com>
2021-05-21 12:13:43 +0700

    Modifikasi fungsi kalkulasi dari database

--- proc/handler.py
+++ proc/handler.py
@@ -1,20 +1,23 @@
 import numpy as np
-import pandas as pd
 
 
 def calc_data(when):
-    from proc.helper import acc_status, displ_status, strn_status
+    from proc.helper import (
+        acc_status,
+        displ_status,
+        strn_status,
+        connect_pg,
+        pg_to_df
+    )
     np.random.seed(101)
 
-    date_rng = pd.date_range(start='1/1/2018', end='1/12/2021', freq='H')
-    df = pd.DataFrame(date_rng, columns=['date'])
-    df["Acceleration"] = np.random.default_rng().uniform(0.1, 1.0, len(date_rng))
-    df["Displacement"] = np.random.default_rng().uniform(1.0, 5.0, len(date_rng))
-    df["Strain"] = np.random.default_rng().uniform(0.01, 0.1, len(date_rng))
+    conn = connect_pg()
+    df = pg_to_df(conn, "SELECT * FROM sensor", ["id", "date", "acceleration", "displacement", "strain"])
+    df = df.set_index("id")
 
-    df["acc_status"] = df[['Acceleration']].apply(lambda x: acc_status(x[0]), axis=1)
-    df["displ_status"] = df[['Displacement']].apply(lambda x: displ_status(x[0]), axis=1)
-    df["strn_status"] = df[['Strain']].apply(lambda x: strn_status(x[0]), axis=1)
+    df["acc_status"] = df[['acceleration']].apply(lambda x: acc_status(x[0]), axis=1)
+    df["displ_status"] = df[['displacement']].apply(lambda x: displ_status(x[0]), axis=1)
+    df["strn_status"] = df[['strain']].apply(lambda x: strn_status(x[0]), axis=1)
 
     if when == "daily":
         df_acc = df.groupby([
