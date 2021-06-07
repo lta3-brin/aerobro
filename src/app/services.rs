603ca0947149956373e74974213e05f822b24a1d
userbaruu <fulan.gulan@gmail.com>
2021-06-07 13:40:05 +0700

    Menambahkan fungsi config database

--- src/app/services.rs
+++ src/app/services.rs
@@ -1,5 +1,7 @@
+use sqlx::PgPool;
 use std::{env, str, io};
-use crate::app::models::{AppErrors, AppConfigs};
+use crate::app::models::{AppErrors, AppConfigs, DbConn};
+use crate::app::helpers::get_configs;
 
 impl From<sqlx::Error> for AppErrors {
     fn from(err: sqlx::Error) -> Self {
@@ -40,9 +42,13 @@ impl From<io::Error> for AppErrors {
 impl AppConfigs {
     pub fn new(addr: String, user: String,
            password: String, topic: String,
-           appport: String
+           appport: String, dbhost: String,
+           dbport: String, dbname: String,
+           dbuser: String, dbpwd: String
     ) -> Self {
-        Self { addr, user, password, topic, appport }
+        Self { addr, user, password, topic, appport,
+            dbhost, dbport, dbname, dbuser, dbpwd
+        }
     }
 
     pub fn get_addr(&self) -> String {
@@ -63,5 +69,42 @@ impl AppConfigs {
 
     pub fn get_app_port(&self) -> String {
         self.appport.clone()
+    }
+
+    pub fn get_dbhost(&self) -> String {
+        self.dbhost.clone()
+    }
+
+    pub fn get_dbport(&self) -> String {
+        self.dbport.clone()
+    }
+
+    pub fn get_dbname(&self) -> String {
+        self.dbname.clone()
+    }
+
+    pub fn get_dbuser(&self) -> String {
+        self.dbuser.clone()
+    }
+
+    pub fn get_dbpwd(&self) -> String {
+        self.dbpwd.clone()
+    }
+}
+
+impl DbConn {
+    pub async fn create() -> Result<PgPool, AppErrors> {
+        let config = get_configs()?;
+        let addr = format!(
+            "postgres://{}:{}@{}:{}/{}",
+            config.get_dbuser(),
+            config.get_dbpwd(),
+            config.get_dbhost(),
+            config.get_dbport(),
+            config.get_dbname()
+        );
+        let pool = PgPool::new(addr.as_str()).await?;
+
+        Ok(pool)
     }
 }
