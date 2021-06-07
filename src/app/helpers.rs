e89baab9c0638f9fdf3b24c1eca18757a0c04809
userbaruu <fulan.gulan@gmail.com>
2021-06-07 13:28:18 +0700

    Penambahan variabel database

--- src/app/helpers.rs
+++ src/app/helpers.rs
@@ -8,7 +8,15 @@ pub fn get_configs() -> Result<AppConfigs, AppErrors> {
     let password = env::var("MQTT_PWD")?;
     let topic = env::var("MSG_TOPIC")?;
     let appport = env::var("APP_PORT")?;
-    let configs = AppConfigs::new(addr, user, password, topic, appport);
+    let dbhost = env::var("DB_HOST")?;
+    let dbport = env::var("DB_PORT")?;
+    let dbname = env::var("DB_NAME")?;
+    let dbuser = env::var("DB_USER")?;
+    let dbpwd = env::var("DB_PASSWORD")?;
+    let configs = AppConfigs::new(
+        addr, user, password, topic, appport,
+        dbhost, dbport, dbname, dbuser, dbpwd
+    );
 
     Ok(configs)
 }
