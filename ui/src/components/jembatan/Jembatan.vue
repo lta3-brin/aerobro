5009dd64fc42df0b494db193eab97c07001f7437
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-07-23 07:58:50 +0700

    Memindahkan lokasi ui ke dalam template

--- ui/src/components/jembatan/Jembatan.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="q-px-md">
-    <canvas ref="jembatan" class="full-width full-height" />
-    <q-resize-observer @resize="onResize" />
-  </div>
-</template>
-
-<script src="./jembatanHandler.js"/>
--- ui/src/store/jembatan/actions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export async function versionAction () {
-  const repo = process.env.REPO_NAME
-  const result = await axios.get(`https://api.github.com/repos/bbta3-bppt/${repo}/tags`)
-
-  return result.data
-}
