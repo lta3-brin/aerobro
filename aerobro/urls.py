4e7bf7d4500f8687f84b0b9046409293a929a95f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-07-26 13:38:27 +0700

    Mendaftarkan route baru untuk halaman index

--- aerobro/urls.py
+++ aerobro/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from aerobro.views import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('stats/', include('stats.urls'))
 ]
