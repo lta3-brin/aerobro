d18ca25e73845a63bce8e9e48038747dc4d065f3
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-07-27 18:41:10 +0700

    Menerapkan dask ke dalam pandas

--- stats/services/convert.py
+++ stats/services/convert.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from dask import dataframe as dd
 
 
 class ConvertService:
     @staticmethod
-    def get_displacement(payloads):
+    def get_displacement(payloads, freq):
         df = pd.DataFrame(
             data=payloads,
             columns=[
@@ -15,36 +16,47 @@ class ConvertService:
             ]
         )
 
+        df = dd.from_pandas(df, npartitions=3)
+
         df['i1'] = df[['ai22_1', 'refvoltage', 'shuntresistor']].apply(
             lambda row: row[0] / (2 ** 17) * row[1] / row[2] * 100 * 1000,
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df['delta1'] = df[['i1', 'offsettrd']].apply(
             lambda row: row[0] - row[1],
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df['displ1'] = df[['delta1', 'slopetrd']].apply(
             lambda row: row[0] * row[1] / 1000 / 100,
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df['i2'] = df[['ai22_2', 'refvoltage', 'shuntresistor2']].apply(
             lambda row: row[0] / (2 ** 17) * row[1] / row[2] * 100 * 1000,
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df['delta2'] = df[['i2', 'offsettrd2']].apply(
             lambda row: row[0] - row[1],
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df['displ2'] = df[['delta2', 'slopetrd2']].apply(
             lambda row: row[0] * row[1] / 1000 / 100,
-            axis=1
+            axis=1,
+            meta=(None, 'float64')
         )
 
         df = df[['logdt', 'displ1', 'displ2']]
+        df = df.set_index('logdt')
+        df = df.resample(freq).max().compute()
+        df = df.reset_index()
 
         return df.to_json(orient='records', date_format='iso')
